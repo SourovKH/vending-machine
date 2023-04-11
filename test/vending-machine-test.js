cb0a59faf15272685011c3d52f2db4fd4b0d27ae
Sourov Kr Halder <sourovkrhalder@Sourovs-MacBook-Pro.local>
2023-04-11 16:03:00 +0530

    One test case of vending machine passed

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -6,5 +6,4 @@ const displayHeader = testing.displayHeader;
 const vendingMachine = vending.vendingMachine;
 
 displayHeader("vendingMachine");
-assertTest(1, vendingMachine(), "1 is equal to 1", "vendingMachine");
-assertTest(2, vendingMachine(), "1 is not equal to 2", "vendingMachine");
+assertTest(1, vendingMachine(1), "For amount one, vending machine should give 1 coin", "vendingMachine");
