8624719ba7ca92dffe1f0f557fa952289518d69e
Sourov <SourovKH@users.noreply.github.com>
2023-04-12 18:20:07 +0530

    coin dispension is working for denominations 1,4,7

--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -14,10 +14,10 @@ assertTest(2, vendingMachine(3, [1, 2]), "For 3 rupees and denominations are (1,
 assertTest(1, vendingMachine(5, [5]), "For 5 rupees and denomination is 5, vending machine should give 1 coins", "vendingMachine");
 assertTest(2, vendingMachine(6, [1, 5]), "For 6 rupees and denominations are (1,5), vending machine should give 2 coins", "vendingMachine");
 assertTest(2, vendingMachine(7, [2, 5]), "For 7 rupees and denominations are (2,5), vending machine should give 2 coins", "vendingMachine");
-assertTest(3, vendingMachine(8, [1, 2, 5]), "For 8 rupees and denominations are (1,2,5), vending machine should give 3 coins", "vendingMachine");
+assertTest(4, vendingMachine(13, [1, 4, 7]), "For 13 rupees and denominations are (1,4,7), vending machine should give 4 coins", "vendingMachine");
 assertTest(1, vendingMachine(10, [10]), "For 10 rupees and denomination is 10, vending machine should give 1 coin", "vendingMachine");
 assertTest(2, vendingMachine(11,[1,2,5,10]), "For 11 ruppees and denominations are (1,2,5,10), vending machine should give 2 coins", "vendingMachine");
-assertTest(2, vendingMachine(15, [1,2,5,10]), "For 15 rupees and denominations are (1,2,5,10), vending machine should give 2 coins", "vendingMachine");
+assertTest(2, vendingMachine(15, [1,4,5,10]), "For 15 rupees and denominations are (1,2,5,10), vending machine should give 2 coins", "vendingMachine");
 assertTest(4, vendingMachine(18, [1,2,5,10]), "For 18 rupees and denomination are (1,2,5,10), vending machine should give 4 coins", "vendingMachine");
 
 displaySummary();
